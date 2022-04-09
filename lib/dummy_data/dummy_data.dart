9cf60586cdcb8b7766e7dee4ac09e2adf99ee363
flutter-coder <4am.library@gmail.com>
2022-04-10 00:53:42 +0900

    Page 연결

--- lib/dummy_data/dummy_data.dart
+++ lib/dummy_data/dummy_data.dart
@@ -2,10 +2,10 @@ import 'package:flutter_gitpage_home/model/book.dart';
 
 class DummyData {
   static final books = [
-    Book(idx: 1, title: "Flutter", src: "https://111coding.github.io/books/gitbook", imgSrc: "assets/img/flutter.jpg"),
-    Book(idx: 2, title: "SwiftUI", src: "https://111coding.github.io/books/gitbook", imgSrc: "assets/img/swiftui.jpg"),
+    Book(idx: 1, title: "Flutter", src: "https://111coding.github.io/books/flutter", imgSrc: "assets/img/flutter.jpg"),
+    Book(idx: 2, title: "SwiftUI", src: "https://111coding.github.io/books/swift", imgSrc: "assets/img/swiftui.jpg"),
     Book(idx: 3, title: "Gitbook", src: "https://111coding.github.io/books/gitbook", imgSrc: "assets/img/gitbook.jpg"),
-    Book(idx: 4, title: "Spring Boot", src: "https://111coding.github.io/books/gitbook", imgSrc: "assets/img/springboot.jpg"),
-    Book(idx: 5, title: "AWS", src: "https://111coding.github.io/books/gitbook", imgSrc: "assets/img/aws.jpg"),
+    Book(idx: 4, title: "Spring Boot", src: "https://111coding.github.io/books/springboot", imgSrc: "assets/img/springboot.jpg"),
+    Book(idx: 5, title: "AWS", src: "https://111coding.github.io/books/aws", imgSrc: "assets/img/aws.jpg"),
   ];
 }
